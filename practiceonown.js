4aeb1b2dd493cbf60bb24427336d8c35712c0563
jalbiston <jacob.albiston@gmail.com>
2021-08-23 20:44:13 -0600

    Study practice on my own

---
+++ practiceonown.js
@@ -0,0 +1,25 @@
+console.log("hello world")
+
+const firstName = "Jake";
+let age = 30;
+console.log(firstName, age )
+
+
+let favSuperhero= "Spiderman"
+let favColor= "Black"
+//concatenate strings with +
+console.log("Hello, My name is " + firstName);
+//Concatenate strings with template literal
+console.log(`I am ${age} years old`)
+
+const isCool = true;
+
+let powerLevel;
+let weaknesses = null;
+console.log(powerLevel)
+
+if (age > 25){
+    console.log("Jake is old")
+} else {
+    console.log("Jake is Young")
+}
